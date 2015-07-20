24f1baa56f8c9121ab5f61c472f1da65e18f1597
vinay427 <vhiremath95@gmail.com>
2015-07-20 17:17:30 -0400

    Copied new favicon to apps/imgs dir

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+////////////////////////////
+
+// Header variables and functions
 var express = require('express'),
 	http = require('http'),
 	app = express(),
@@ -9,7 +12,7 @@ if (typeof ipaddress === "undefined") {
 	ipaddress = "127.0.0.1";
 	console.warn('No OPENSHIFT_NODEJS_IP var, using ' + ipaddress + ':8000');
 }
-//console.log(__dirname);
+//console.log(__(;
 
 app.set('views', 'app/jade/jadepages');
 app.set('view engine', 'jade');
@@ -17,57 +20,17 @@ app.set('port', port);
 app.use('/', express.static('app/'));
 app.use(bodyParser.json());
 
+// Function to render requested pages
 function render(path, page){
-	app.get(path, function(req, resp){
-		resp.render(page);
-	});
+        app.get(path, function(req, resp){
+                resp.render(page);
+        });
 }
 
-function signUp(name, email, desc, callback){
-	var MailChimpAPI = require('mailchimp').MailChimpAPI,
-		API_KEY = 'feff5c1e72c5294f3a3564f2c5c42386-us6',
-		LIST_ID = '7a6ce43042';
-	var api = new MailChimpAPI(API_KEY , { version : '1.3', secure : false }),
-		names = name.split(' '), status,
-		data = {
-			apikey: API_KEY,
-			id: LIST_ID,
-			email_address : email,
-			merge_vars: {'FNAME': names[0], 'LNAME': names[1], 'MMERGE3' : desc},
-			double_optin: false,
-			send_welcome: true
-		};
-	 api.listSubscribe(data, function(error, result) {
-		if (error) {
-			console.log(error);
-			callback(error);
-		} else {
-			callback(null, result);
-		}
-	});
-}
+////////////////////////////
 
-//cache set
-app.use(express.static(__dirname + '/imgs', { maxAge: 86400000 }));
-app.use(express.static(__dirname + '/css', { maxAge: 86400000 }));
-app.use(express.static(__dirname + '/js', { maxAge: 86400000 }));
-
+// Pages to render (located in ./app/jade/jadepages)
 render('/','index');
-
-app.post('/subscribe', function(req, res){
-	var name = req.body.name, email = req.body.email, desc = req.body.desc;
-	signUp(name,email, desc, function(err, result){
-		if(err){
-			res.locals.signerror="error";
-			res.redirect('http://optimizemichigan.org');
-			return;
-		}
-		res.locals.signerror="success";
-		res.redirect('http://optimizemichigan.org');
-	});
-});
-
-// Paths with matching pages in ./app/jade/jadepages
 render('/optimizeteam','optimizeteam');
 render('/about','about');
 render('/teams','teams');
@@ -79,9 +42,53 @@ render('/social','social');
 render('/courses','courses');
 render('/resources', 'resources');
 
+// Cache assets
+app.use(express.static(__dirname + '/imgs', { maxAge: 86400000 }));
+app.use(express.static(__dirname + '/css', { maxAge: 86400000 }));
+app.use(express.static(__dirname + '/js', { maxAge: 86400000 }));
+
+app.post('/subscribe', function(req, res){
+        var name = req.body.name, email = req.body.email, desc = req.body.desc;
+        signUp(name,email, desc, function(err, result){
+                if(err){
+                        res.locals.signerror="error";
+                        res.redirect('http://optimizemichigan.org');
+                        return;
+                }
+                res.locals.signerror="success";
+                res.redirect('http://optimizemichigan.org');
+        });
+});
+
+
+// Function for newsletter signups 
+function signUp(name, email, desc, callback){
+        var MailChimpAPI = require('mailchimp').MailChimpAPI,
+                API_KEY = 'feff5c1e72c5294f3a3564f2c5c42386-us6',
+                LIST_ID = '7a6ce43042';
+        var api = new MailChimpAPI(API_KEY , { version : '1.3', secure : false }),
+                names = name.split(' '), status,
+                data = {
+                        apikey: API_KEY,
+                        id: LIST_ID,
+                        email_address : email,
+                        merge_vars: {'FNAME': names[0], 'LNAME': names[1], 'MMERGE3' : desc},
+                        double_optin: false,
+                        send_welcome: true
+                };
+         api.listSubscribe(data, function(error, result) {
+                if (error) {
+                        console.log(error);
+                        callback(error);
+                } else {
+                        callback(null, result);
+                }
+        });
+}
+
 // 404 page in ./app/jade/jadepages
 app.use(function(req, res) {
-	res.render('error');
+        res.render('error');
 });
 
 // Start the server
